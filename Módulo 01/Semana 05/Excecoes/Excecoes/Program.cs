68b2cbb700eaaf0fe24e4376711d5020918ef033
tiago-egas <tiago.egas@outlook.com>
2023-02-28 00:51:02 -0300

    Semana 05 start

---
+++ Módulo 01/Semana 05/Excecoes/Excecoes/Program.cs
@@ -0,0 +1,23 @@
+﻿try // Tente
+{
+    // Código
+    Console.Write("Digite um número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"O resultado de {numero} x 10 é: {numero * 10}.");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine($"\n\nErro específico capturado: {ex}.\n\nCausa do erro: {ex.Message}.\n\n");
+}
+
+catch (Exception ex) // Trate
+{
+    // Código do tratamento do erro
+    Console.WriteLine($"Erro capturado: {ex.Message}.\n\n");
+}
+finally // Finalmente
+{
+    // Código da "limpeza" final
+    Console.WriteLine("Somente um números.\n\n");
+}
---
+++ Módulo 01/Semana 05/Excecoes2/Excecoes2/Program.cs
@@ -0,0 +1,26 @@
+﻿int x = 2;
+int y = 0;
+
+try
+{
+    Console.WriteLine($"x / y é igual a {x / y}.");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine($"\n\nExplicação do erro de divisão por zero: {ex.Message}");
+    Console.WriteLine($"\n\nStackTrace do erro: {ex.StackTrace}");
+}
+catch (ArithmeticException ex)
+{
+    Console.WriteLine($"\n\nExplicação do erro: {ex.Message}");
+    Console.WriteLine($"\n\nStackTrace do erro: {ex.StackTrace}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"\n\nExplicação do erro genérico: {ex.Message}");
+    Console.WriteLine($"\n\nStackTrace do erro: {ex.StackTrace}");
+}
+finally
+{
+    
+}
